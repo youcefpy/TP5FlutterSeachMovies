429aecc53655e977cf7df6b9e59f90914bf82806
Youcef <youcefboutiche@hotmail.fr>
2024-10-20 13:58:15 +0200

    filtring the comments by movieId

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import './screens/tab_screen.dart';
 import 'package:provider/provider.dart';
 import './providers/favorite_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/screens/details_movie.dart
+++ lib/screens/details_movie.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/favorite_provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/intl.dart';
 
 class MovieDetailPage extends StatefulWidget {
   // final Map movie;
@@ -18,13 +19,58 @@ class MovieDetailPage extends StatefulWidget {
 class _MovieDetailPageState extends State<MovieDetailPage> {
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _reviewController = TextEditingController();
-    final TextEditingController _ratingController = TextEditingController();
     final movie =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
-    print("Movie data: $movie");
     final favoriteMovieProvider = Provider.of<FavorieMovie>(context);
     final isFavorite = favoriteMovieProvider.isfavorite(movie);
+    //text controller review
+    final TextEditingController _reviewController = TextEditingController();
+    // text controller ratting
+    final TextEditingController _ratingController = TextEditingController();
+
+    //collection referance => get the doc "reviews where i show the comments"
+    final fetchData = FirebaseFirestore.instance
+        .collection("reviews")
+        .where("movieId", isEqualTo: movie['imdbID']);
+
+    void insertDataInFb() async {
+      //stock the text in the variable review
+      final review = _reviewController.text.trim();
+      //stock ratting in rattingText
+      final ratingText = _ratingController.text.trim();
+      //convert ratting from text (string) to int
+      final rating = int.tryParse(ratingText);
+      //check if the review field in empty or ratting is null or lt 1 or gt5
+      if (review.isEmpty || rating == null || rating < 1 || rating > 5) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Invalide entrée! SVP resaisir encore."),
+          ),
+        );
+        return;
+      }
+
+      try {
+        //init firebaseFireStore to store the data in the doc review
+        await FirebaseFirestore.instance.collection('reviews').add({
+          'movieId': movie['imdbID'],
+          'avis': review,
+          'rating': rating,
+          'timestamp': FieldValue.serverTimestamp(),
+        });
+        //showing a message in the snack bar
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Avis envoyé avec succès!")),
+        );
+        //clear the fields
+        _reviewController.clear();
+        _ratingController.clear();
+      } catch (error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Erreur lors de l'envoi de l'avis!")),
+        );
+      }
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -92,7 +138,76 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
                 ),
               ),
               SizedBox(
-                height: 10,
+                height: 30,
+              ),
+              //ajouter un streamBuilder
+              StreamBuilder(
+                  stream: fetchData.snapshots(),
+                  builder:
+                      (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                    if (streamSnapshot.hasData) {
+                      return ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: streamSnapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            final DocumentSnapshot documentSnapshot =
+                                streamSnapshot.data!.docs[index];
+                            final timestamp = documentSnapshot['timestamp'];
+                            DateTime dateTime;
+
+                            if (timestamp is Timestamp) {
+                              dateTime = timestamp.toDate();
+                            } else if (timestamp is String) {
+                              dateTime = DateTime.parse(timestamp);
+                            } else {
+                              dateTime = DateTime.now();
+                            }
+
+                            return Material(
+                              child: ListTile(
+                                leading: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      DateFormat('dd-MM-yyyy').format(dateTime),
+                                      style: TextStyle(fontSize: 12),
+                                    ),
+                                    Text(
+                                      DateFormat("HH-mm").format(dateTime),
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                title: Text(
+                                  documentSnapshot['avis'],
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.orange,
+                                      size: 16,
+                                    ),
+                                    Text(
+                                      documentSnapshot['rating'].toString(),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                    }
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }),
+              SizedBox(
+                height: 30,
               ),
               TextField(
                 controller: _reviewController,
@@ -109,45 +224,7 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
                 height: 10,
               ),
               ElevatedButton(
-                onPressed: () async {
-                  final review = _reviewController.text.trim();
-                  final ratingText = _ratingController.text.trim();
-                  final rating = int.tryParse(ratingText);
-
-                  // Validate inputs
-                  if (review.isEmpty ||
-                      rating == null ||
-                      rating < 1 ||
-                      rating > 5) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Invalid entrée! SVP resaisir encore."),
-                      ),
-                    );
-                    return;
-                  }
-
-                  try {
-                    await FirebaseFirestore.instance.collection('reviews').add({
-                      'movieId': movie['imdbID'],
-                      'avis': review,
-                      'rating': rating,
-                      'timestamp': FieldValue.serverTimestamp(),
-                    });
-
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Avis envoyé avec succès!")),
-                    );
-
-                    _reviewController.clear();
-                    _ratingController.clear();
-                  } catch (error) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text("Erreur lors de l'envoi de l'avis!")),
-                    );
-                  }
-                },
+                onPressed: () => insertDataInFb(),
                 child: const Text("Soumettre Avis"),
               ),
             ],
